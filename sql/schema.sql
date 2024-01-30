89d2e4ebc6dc72367aa5f14abdcd89ef718d2ab8
Brandon Corey <bcorey3660@gmail.com>
2024-01-30 18:55:03 -0500

    changes schema to be loser and adds hash to request for endpont

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,14 +1,15 @@
 CREATE TABLE endpoints (
   endpoint_id serial PRIMARY KEY,
-  endpoint_hash varchar(8) NOT NULL,
+  endpoint_hash text NOT NULL,
   endpoint_name text NOT NULL,
   created_at timestamp DEFAULT NOW()
 );
 
 CREATE TABLE requests (
   request_id serial PRIMARY KEY,
-  request_hash varchar(16) NOT NULL,
+  request_hash text NOT NULL,
   endpoint_id integer REFERENCES Endpoints(endpoint_id) ON DELETE CASCADE,
+  endpoint_hash text NOT NULL,
   document_id integer,
   http_method varchar(10) NOT NULL,
   http_path text NOT NULL,
