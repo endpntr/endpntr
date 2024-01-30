23bcdf9908f9d6e91a5a8ebc50126d1a3e95d7e9
Brandon Corey <bcorey3660@gmail.com>
2024-01-29 20:53:03 -0500

    adds catch error function to main app file

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 "use strict";
 
-const express   = require("express");
+const express = require("express");
 const subdomain = require("express-subdomain");
-const morgan    = require("morgan");
+const morgan = require("morgan");
 
-const config  = require("./lib/config");
+const config = require("./lib/config");
 const webhook = require("./lib/middleware").webhooksMiddleware;
-const errors  = require("./lib/middleware").errors;
+const errors = require("./lib/middleware").errors;
+const catchError = require("./lib/catch-error");
 
-const app  = express();
+const app = express();
 const PORT = config.PORT;
 
 app.set("views", "./views");
@@ -19,20 +20,21 @@ const endpoint = subdomain("*.req", (req, res, next) => {});
 app.use(morgan("common"));
 app.use(endpoint);
 
-app.get("/", (req, res) => res.render("index")); 
-app.post("/createEndpoint",              catchError(webhook.createNewEndpoint));
-app.get("/req/:endpoint_id",             catchError(webhook.viewEndpoint)); 
+app.get("/", (_, res) => res.render("index"));
+app.post("/createEndpoint", catchError(webhook.createNewEndpoint));
+app.get("/req/:endpoint_id", catchError(webhook.viewEndpoint));
 app.get("/req/:endpoint_id/:request_id", catchError(webhook.viewRequest));
 
+// CHANGE BACK TO POST AFTER
 app.post("/", catchError(webhook.processRequest));
 
 // Ignore Favicon
-app.get('/*', errors.handleFavicon);
+app.get("/*", errors.handleFavicon);
 
 // Catch-all error handler
 app.use(errors.generalErrorHandler);
 
 // attach the `res` subdomain to the main app
-app.use(subdomain('req', app));
-app.use(subdomain('*', app));
+app.use(subdomain("req", app));
+app.use(subdomain("*", app));
 app.listen(PORT, () => console.log("Team08 RequestBin clone is running..."));
