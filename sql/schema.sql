dcee49a246e3820e2f1579426f6ee79297ea6cf1
Brandon Corey <bcorey3660@gmail.com>
2024-01-29 19:01:35 -0500

    schema and seed conflicts resolved

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,15 +1,15 @@
 CREATE TABLE Endpoints (
   endpoint_id serial PRIMARY KEY,
   endpoint_hash VARCHAR(8) NOT NULL,
-  created_at timestamp DEFAULT NOW(),
+  created_at timestamp DEFAULT NOW()
 );
 
 CREATE TABLE Requests (
   request_id serial PRIMARY KEY,
   request_hash VARCHAR(16) NOT NULL,
-  endpoint_id FOREIGN KEY REFERENCES Endpoints(endpoint_id) ON DELETE CASCADE,
+  endpoint_id integer REFERENCES Endpoints(endpoint_id) ON DELETE CASCADE,
   document_id integer,
   http_method VARCHAR(10) NOT NULL,
   http_path TEXT NOT NULL,
-  received_at timestamp DEFAULT NOW(),
+  received_at timestamp DEFAULT NOW()
 );
--- sql/seed-data.sql
+++ sql/seed-data.sql
@@ -1,12 +1,12 @@
-INSERT INTO endpoints(endpoint_hash, created_at) VALUES
+INSERT INTO endpoints(endpoint_hash) VALUES
   ('q0Y06dR8'),
   ('Qf8yrY5f'),
   ('PTVeEMLQ');
 
 INSERT INTO requests(request_hash, endpoint_id, http_method, http_path) VALUES 
-  ('AickKFLV0ILYhffE', '1', 'GET', '/request1'),
-  ('DyqFQDwoUzWDaDbW', '1', 'POST', '/request2'),
-  ('vQBC6W3g6KDc4JIE', '2', 'GET', '/request3'),
-  ('0Ni28ss1ZsoEqZcE', '2', 'POST', '/request4'),
-  ('3ttdvdlOZTD6aOxj', '3', 'GET', '/request5'),
-  ('Lo6pl0Rsb7IV7iBY', '3', 'POST', '/request6');
+  ('AickKFLV0ILYhffE', 1, 'GET', '/request1'),
+  ('DyqFQDwoUzWDaDbW', 1, 'POST', '/request2'),
+  ('vQBC6W3g6KDc4JIE', 2, 'GET', '/request3'),
+  ('0Ni28ss1ZsoEqZcE', 2, 'POST', '/request4'),
+  ('3ttdvdlOZTD6aOxj', 3, 'GET', '/request5'),
+  ('Lo6pl0Rsb7IV7iBY', 3, 'POST', '/request6');
