1ab24ce16d1c2fdbc7f06f9b77ad66f630889a57
Brandon Corey <bcorey3660@gmail.com>
2024-02-01 03:32:54 -0500

    adds ability to fetch payload on click

--- sql/seed-data.sql
+++ sql/seed-data.sql
@@ -6,10 +6,10 @@ INSERT INTO endpoints(endpoint_hash, endpoint_name) VALUES
   ('hawk-proven-curiously', 'default5'),
   ('painfully-singular-chow', 'default6');
 
-INSERT INTO requests(request_hash, endpoint_id, endpoint_hash, http_method, http_path) VALUES 
-  ('AickKFLV0ILYhffE', 1, 'q0Y06dR8', 'GET', '/'),
-  ('DyqFQDwoUzWDaDbW', 1, 'q0Y06dR8', 'POST', '/'),
-  ('vQBC6W3g6KDc4JIE', 2, 'Qf8yrY5f', 'GET', '/'),
-  ('0Ni28ss1ZsoEqZcE', 2, 'Qf8yrY5f', 'POST', '/'),
-  ('3ttdvdlOZTD6aOxj', 4, 'super-gobbler-robust', 'GET', '/'),
-  ('Lo6pl0Rsb7IV7iBY', 4, 'super-gobbler-robust', 'POST', '/');
+INSERT INTO requests(request_hash, endpoint_id, endpoint_hash, http_method, http_path, document_id) VALUES 
+  ('AickKFLV0ILYhffE', 1, 'q0Y06dR8', 'GET', '/', '1234'),
+  ('DyqFQDwoUzWDaDbW', 1, 'q0Y06dR8', 'POST', '/', '123'),
+  ('vQBC6W3g6KDc4JIE', 2, 'Qf8yrY5f', 'GET', '/', '132'),
+  ('0Ni28ss1ZsoEqZcE', 2, 'Qf8yrY5f', 'POST', '/', '123'),
+  ('3ttdvdlOZTD6aOxj', 4, 'super-gobbler-robust', 'GET', '/', '1234'),
+  ('Lo6pl0Rsb7IV7iBY', 4, 'super-gobbler-robust', 'POST', '/', '65bb4cf5aff54d4cb02e9ca8');
